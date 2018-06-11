81cc53783b05ae37213c601e536681ee775d5eee
vitormelo95 <vitorbotmelo95@gmail.com>
2018-06-10 22:55:12 -0300

    atualização das funções

--- router.py
+++ router.py
@@ -7,6 +7,7 @@ from threading import *
 
 PORT = 55151
 MAX = 4096
+EXIT = Event()
 
 class Message:
     type = '' #o tipo deve ser: 'data' | 'update' | 'trace'
@@ -35,27 +36,29 @@ class Message:
 class Router:
      #endereço do roteador 
      #endereços de rotas para o roteador acima [ {addr:'1.1.1.1', dist: 10, last_update: 0 }, ...]  
-    def __init__(self, address, route_addr, dist, learn_from):
+    def __init__(self, address, route_addr, dist):
         self._address = address
         self._routes = []
-        self.add_route(route_addr,dist, learn_from)
+        self.add_route(route_addr,dist)
     
-    def add_route(self, route_addr, dist, learn_from):
-        route = { 'addr' : route_addr , 'dist': dist, 'last_update': 0, 'learn_from': learn_from  }
+    def add_route(self, route_addr, dist):
+        route = { 'addr' : route_addr , 'dist': dist, 'last_update': 0 }
         self._routes.append(route) 
 
-    def update_route(self, route_addr, dist, learn_from):
+    def update_route(self, route_addr, dist):
         find = False
+        #atualiza rota para o roteador, se a rota não existir adiciona ela
+        #o programa mantém todas as rotas conhecidas por mensagem de update
         for route in self._routes:
             if(route['addr'] == route_addr):
                 route['dist'] = dist
                 route['last_update'] = 0
-                route['learn_from'] = learn_from
                 find = True
         if(not find):
-            self.add_route(route_addr,dist, learn_from)
+            self.add_route(route_addr,dist)
         
     def remove_route(self,route_addr):
+        #remove a rota que passa pelo endereço passado
         for route in self._routes:
             if(route['addr'] == route_addr):
                 self._routes.remove(route)
@@ -66,12 +69,15 @@ class Router:
         return False
     
     def is_neigh(self):
+        #se o endereço do roteador for igual ao de um caminho o roteador é vizinho
         for route in self._routes:
             if(route['addr'] == self._address):
                 return True
         return False
 
     def get_route_addr(self):
+        #retorna o endereço do próximo roteador
+        #se tiver mais de uma rota com a mesma distância é feito um sorteio 
         min_dist = 0
         first = True
         routes = []
@@ -86,17 +92,20 @@ class Router:
                     routes = [route['addr']]
                 if (min_dist == route['dist']):
                     routes.append(route['addr'])
-        print(routes)
         n = random.randint(0, len(routes)-1)
         return routes[n]
     
     def get_dist_dict(self, to_addr):
+        #utilizada na formação do vetor de update 
+        #verifica qual endereço estamos enviando a mensagem
+        #retorna a menor distância conhecida que não passa por quem estamos enviando
         min_dist = -1
         first = True
         for route in self._routes:
-            if(to_addr != route['learn_from']):
+            if(to_addr != route['addr']):
                 if(first):
                     min_dist = route['dist']
+                    first = False
                 elif (min_dist > route['dist'] ):
                     min_dist = route['dist']
         if ( min_dist > 0 ):
@@ -104,6 +113,7 @@ class Router:
         return {}
     
     def get_dist(self):
+        #pega a menor distância para o roteador
         min_dist = -1
         first = True
         for route in self._routes:
@@ -113,94 +123,118 @@ class Router:
             elif (min_dist > route['dist'] ):
                 min_dist = route['dist']
         return min_dist
+    
+    def incr_last_update(self):
+        #incrementa o last_update, se ele for maior que três remove a rota
+        for route in self._routes:
+            if(self._address != route['addr']):
+                route['last_update'] += 1
+                if(route['last_update'] > 3):
+                    self._routes.remove(route)
 
 class Routers: 
-     #vetor de roteadores (class Router) para armazenar a lista de roteadores 
-     #e definir as funções para utiliza-lo
+     #dicionário de roteadores {'addrs': Router, ...} para armazenar a lista de roteadores e rotas 
+     #define as funções para utilizar a lista
     def __init__(self):
-        self._routers = []
+        self._routers = {}
 
     def add(self, router):
-        self._routers.append(router)
+        self._routers.update({ router._address: router})
 
     def update(self, addr, route_addr, dist):
-        find = False
-        for router in self._routers:
-            if(router._address == addr):
-                router.update_route(route_addr, dist, route_addr)
-                find = True
-        if( not find):
-            router = Router(addr,route_addr,dist,route_addr)
+        #atualiza as rotas dos roteadores, se o roteador não for conhecido adiciona ele
+        if( addr in self._routers):
+            self._routers[addr].update_route(route_addr, dist)
+        else:
+            router = Router(addr,route_addr, dist)
             self.add(router)
         
     def remove(self, addr):
-        for router in self._routers:
-            if(router._address == addr):
-                router.remove_route(addr)
-                if(not router.exist_routes()):
-                    self._routers.remove(router)
+        #remove todas as rotas aprendidas pelo endereço deletado
+        for router_addr,route in self._routers.items():
+            route.remove_route(addr)
             
     def get_distances(self, to_addr):
+        #monta o dicionário de distâncias para o roteador (to_addr)
         distances = {}
-        for router in self._routers:
-            if(router._address != to_addr):
+        for addr,router in self._routers.items():
+            if(addr!= to_addr):
                 distances.update(router.get_dist_dict(to_addr))
         return distances
 
     def get_router_route(self,dest_addr):
-        for router in self._routers:
-            if (router._address == dest_addr):
-                return router.get_route_addr()
+        #pega o próximo roteador para chegar em no roteador do endereço passado
+        if( dest_addr in self._routers):
+            return self._routers[dest_addr].get_route_addr()
         return ''
     
     def get_router_dist(self,dest_addr):
-        for router in self._routers:
-            if (router._address == dest_addr):
-                return router.get_dist()
-    
+        #pega a distância para o roteador (dest_addr)
+        if( dest_addr in self._routers):
+            return self._routers[dest_addr].get_dist()
+
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)      
-
+            sort_keys=True, indent=4)       
 
 def send_message(message, addr):
-    if(addr == '' or addr == None ): 
-        print('no route found')
+    #envia a mensagem, addr é o endereço do próximo roteador
+    if(addr == '' or addr == None ):
+        print('no route found to: '+ message.destination)
         return
     with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as sock:
         message_txt = message.toJSON()
         message_txt = message_txt.encode('ascii')
-        print(message_txt)
+        print('>> SENDING: ' + str(message_txt), file=sys.stderr)
         sock.sendto(message_txt,(addr,PORT))
 
 def send_updates(r, this_addr, PERIOD):
-    #essa função deve iterar sobre os vizinhos e enviar a tabela de para cada
-    for router in r._routers:
+    #essa função itera sobre os roteadores conhecidos e monta uma mensagem de update para cada roteador vizinho
+    if EXIT.is_set():
+        sys.exit()
+    for addr,router in r._routers.items():
         if(router.is_neigh()):
-            distances = { this_addr : 0}
-            distances.update(r.get_distances(router._address))
-            message = Message('update',this_addr, router._address, distances = distances)   
-            send_message(message, router._address)
+            distances = { this_addr : 0}#inicia o dicionário com o próprio endereço
+            #atualiza o dicionário com as distâncias conhecidas
+            distances.update(r.get_distances(addr))
+            message = Message('update',this_addr, addr, distances = distances)   
+            send_message(message, addr)
     t = Timer(PERIOD, send_updates, args = (r, ADDR, PERIOD)).start()
 
+def incr_routes(r, PERIOD):
+    #incrementa o campo last_update de todas as rotas conhecidas
+    try:
+        if EXIT.is_set():
+            sys.exit()
+        for addr,router in r._routers.items():
+            router.incr_last_update()
+    except Exception as e:
+        print(str(e),file=sys.stderr)
+    
+    Timer(PERIOD, incr_routes, args = (r,PERIOD)).start()
+
 def ask_trace(r, destination,this_addr):
+    #cria uma mensagem de trace e envia
     message = Message('trace',this_addr, destination,hops = [this_addr])
     next_router = r.get_router_route(destination)
     send_message(message, next_router)
 
 def resp_trace(r,this_addr, message):
+    #se a mensagem de trace for para o roteador responde ela com uma mensagem de data
     message['hops'].append(this_addr)
     resp_message = Message('data',this_addr,message['source'], payload = message)
     next_router = r.get_router_route(resp_message.destination)
     send_message(resp_message, next_router)
 
 def pass_trace(r,this_addr, message):
+    #adiciona o endereço do roteador no hops e repassa a mensagem ao destino
     message = Message(message['type'],message['source'],message['destination'],hops = message['hops'])    
     message.hops.append(this_addr)
     next_router = r.get_router_route(message.destination)
     send_message(message, next_router)
 
 def pass_message(r,message):
+    #repassa uma mensagem recebida ao seu destino
     if(message['type'] == 'data'):
         message = Message(message['type'],message['source'],message['destination'],payload = message['payload'])
     else:
@@ -209,47 +243,63 @@ def pass_message(r,message):
     send_message(message, next_router)
 
 def update_routers(r, message, this_addr):
-    distances = message['distances']
-    print(distances)
-    dist_source = r.get_router_dist(message['source'])   
-    for addr,dist in distances.items():
-        if( addr != this_addr):
-            r.update(addr, message['source'], dist+dist_source)
+    #itera sobre o dicionário de distâncias recebido na mensagem e atualiza as rotas e distâncias
+    try:
+        distances = message['distances']
+        source_addr = message['source']
+        if( source_addr in r._routers and r._routers[source_addr].is_neigh() ): 
+            #verifica se o roteador de origem da mensagem é vizinho para aceitar o update
+            dist_source = r.get_router_dist(source_addr)
+            for addr,dist in distances.items():
+                if( addr != this_addr):
+                    r.update(addr, message['source'], dist+dist_source)
+    except Exception as e:
+        print(str(e),file=sys.stderr)
+    
 
 def process_command(r, text, this_addr):
-    # essa função deve verificar o comando recebido e executa-lo
-    text = text.split()
-    comm = text[0]
-    #print(text)
-    if( comm  ==  'add'):
-        router = Router(text[1],text[1],int(text[2]),text[1])
-        r.add(router)
-        print(r.toJSON())
-    elif( comm == 'del'):
-        r.remove(text[1])
-    elif (comm == 'trace'):
-        ask_trace(r,text[1],this_addr)
-    elif (comm == 'quit'):
-        print(r.toJSON())
-        sys.exit()
+    # essa função verifica o comando recebido e executa-lo
+    try:
+        text = text.split()
+        comm = text[0]
+        if (comm  ==  'add'):
+            router = Router(text[1],text[1],int(text[2]))
+            r.add(router)
+        elif (comm == 'del'):
+            r.remove(text[1])
+        elif (comm == 'trace'):
+            ask_trace(r,text[1],this_addr)
+        elif (comm == 'quit'):
+            print(r.toJSON())
+            print('Ending...')
+            EXIT.set()
+            sys.exit()
+        elif (comm == 'print'):
+            print(r.toJSON())
+    except Exception as e:
+        print(str(e),file=sys.stderr)
+   
 
 def process_message(r, message, this_addr):
-    print(message)
-    if(message['destination'] == this_addr ):
-        if (message['type'] == 'data'):
-            print( message['payload'])
-        elif (message['type'] == 'update' ):
-            update_routers(r,message, this_addr)
-            print('update')
-            print(r.toJSON())
+    #verifica o tipo da mensagem recebida e chama uma função para trata-lá
+    try:
+        if(message['destination'] == this_addr ):
+            if (message['type'] == 'data'):
+                print( message['payload'])
+            elif (message['type'] == 'update' ):
+                update_routers(r,message, this_addr)
+            elif (message['type'] == 'trace'):
+                resp_trace(r, this_addr, message)
         elif (message['type'] == 'trace'):
-            resp_trace(r, this_addr, message)
-    elif (message['type'] == 'trace'):
-        pass_trace(r,this_addr,message) #adicionar this_addr in hops
-    else:
-        pass_message(r,message)
+            pass_trace(r,this_addr,message) 
+        else:
+            pass_message(r,message)
+    except Exception as e:
+        print(str(e),file=sys.stderr)
+    
 
 def read_file(r, file_name, this_addr):
+    #ler os comandos de um arquivo
     with open(file_name, 'r') as f:
         lines = f.readlines()
         for line in lines:
@@ -257,6 +307,7 @@ def read_file(r, file_name, this_addr):
 
 
 class read_commands(Thread):
+    #essa classe executa em uma thread o recebimento de comandos dos usuários
     def __init__(self, r, this_addr):
         Thread.__init__(self)
         self.r = r
@@ -269,6 +320,7 @@ class read_commands(Thread):
             process_command(self.r,command,self.this_addr)
         
 class receive_messages(Thread):
+    #essa classe executa em uma thread o recebimento de mensagens
     def __init__(self, r, ADDR, PORT):
         Thread.__init__(self)
         self.r = r
@@ -279,31 +331,46 @@ class receive_messages(Thread):
     def run(self):
         with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as sock:
             sock.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.settimeout(2)
             sock.bind((self.addr, self.port))
             while True:
-                data, addr_from = sock.recvfrom(MAX)
-                data = data.decode()
-                process_message(self.r,json.loads(data),self.addr)
+                if EXIT.is_set():
+                    sys.exit()
+                try:
+                    data, addr_from = sock.recvfrom(MAX)
+                    data = data.decode()
+                    print('<< RECEIVING: ' + str(data), file=sys.stderr)
+                    #quando uma mensagem é recebida chama a função em outra thread para processa-la
+                    Thread(target=process_message,args=(self.r,json.loads(data),self.addr)).start()
+                except Exception as e:
+                    print(str(e),file=sys.stderr)
+                    
+                
 
 if __name__ == '__main__':
     if(len(sys.argv) < 3):
         print("Wrong initialization expected parameters <ADDR> <PERIOD>")
         sys.exit(-1)
     
-    ADDR = sys.argv[1]
-    sys.stdout = open('output_'+ADDR, 'w')
-    sys.stderr = open('error_'+ADDR, 'w')
-    PERIOD = float(sys.argv[2])
-
     r = Routers()
+    ADDR = ''
+    PERIOD = ''
+    if (sys.argv[1] == '--addr' ):
+        ADDR = sys.argv[2]
+        PERIOD = float(sys.argv[4])
+        if(len(sys.argv) > 6):
+            read_file(r, sys.argv[6],ADDR)
+    else:
+        ADDR = sys.argv[1]
+        PERIOD = float(sys.argv[2])
+        if(len(sys.argv) > 3):
+            read_file(r, sys.argv[3],ADDR)
+
     
-    if(len(sys.argv) > 3):
-        read_file(r, sys.argv[3],ADDR)
-
+    sys.stderr = open('logs_'+ADDR, 'w')
+    
     read_commands(r,ADDR)
-
     receive_messages(r,ADDR,PORT)
-
-    t = Timer(PERIOD, send_updates, args = (r, ADDR,PERIOD)).start()
-
+    Timer(PERIOD, send_updates, args = (r, ADDR,PERIOD)).start()
+    Timer(PERIOD, incr_routes, args = (r,PERIOD)).start()
  
